82a172019524eadf265d8ea9635cb981dbe4513b
VIVEK BHANDARI <ervivekbhandari@gmail.com>
2020-10-10 16:01:13 +0530

    Added Enemy and the waypoints for enemy to move on.

---
+++ Assets/Scripts/EnemyPath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPath : MonoBehaviour
+{
+    [SerializeField] List<Waypoints> waypoints;
+    const float nextPointTimeDelay = 1f;
+
+    private void Start()
+    {
+        StartCoroutine(GetPath());
+    }
+
+    IEnumerator GetPath()
+    {
+        foreach(Waypoints waypoint in waypoints)
+        {
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(nextPointTimeDelay);
+        }
+    }
+}
