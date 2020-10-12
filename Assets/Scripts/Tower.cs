159466a76b83f2935127beda3c3dc004fe8e8c7b
VIVEK BHANDARI <ervivekbhandari@gmail.com>
2020-10-12 17:33:28 +0530

    Towers added and facing towards the enemy as it moves.

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] Transform towerGun;
+    [SerializeField] Transform enemy;
+
+    void Update()
+    {
+        towerGun.LookAt(enemy);
+    }
+}
