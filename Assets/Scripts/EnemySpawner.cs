0312255d92dd21d9d93326919a32f8211509c357
VIVEK BHANDARI <ervivekbhandari@gmail.com>
2020-10-15 10:16:15 +0530

    Spawning Enemies.

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] float spawnDelay = 10f;
+    [SerializeField] EnemyPath enemyPrefab;
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while (true)
+        {
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+}
