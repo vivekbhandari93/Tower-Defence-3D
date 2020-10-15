59da90d5199b9e6a3a7e8518581f565180c4b0a1
VIVEK BHANDARI <ervivekbhandari@gmail.com>
2020-10-15 18:16:09 +0530

    Added HIT and DEATH Partical VFX.

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class EnemyDamage : MonoBehaviour
 {
     [SerializeField] int hitPoints = 50;
+    [SerializeField] ParticleSystem hitVFX;
+    [SerializeField] ParticleSystem deathVFXPrefab;
 
     private void OnParticleCollision(GameObject other)
     {
         hitPoints -= 1;
+        hitVFX.Play();
         if (hitPoints <= Mathf.Epsilon)
         {
             Destroy(gameObject);
+            var deathVFXInstance = Instantiate(deathVFXPrefab, transform.position, Quaternion.identity);
+            deathVFXInstance.Play();
         }
     }
 }
